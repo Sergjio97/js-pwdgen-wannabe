fe65e4e0a03389c5af93d88118b98c458409d2af
Sergjio97 <sergiocizmja@gmail.com>
2021-11-04 15:52:25 +0100

    fine esercizio

---
+++ js/myscript.js
@@ -0,0 +1,69 @@
+// document.getElementById("title").innerHTML = "Hello world";
+
+// document.getElementById("second-title").innerHTML = "Hello world";
+
+// const myVar = "Ciao";
+
+// myVar = 5;
+
+// console.log(myVar)
+
+// let variable = "Pippo";
+// let variableNew = "Pluto";
+
+// variableNew = "Paperino"
+
+// console.log(variableNew)
+
+// let a = "Ciao"
+// let b = "a tutti!"
+// let c;
+
+// b = a + " " + b;
+// c= a + " " + b;
+
+// console.log(c)ù
+
+
+// let a = "Qui";
+// let b = "Qua";
+
+// console.log(
+//     `
+//     ${a} Quo ${b}
+//     Andarono al mercato
+
+//     `
+// )
+
+// let somma = 4 + 4;
+
+// let sottrazione = somma - 3;
+// console.log(sottrazione)
+
+// const a = prompt();
+// console.log(a);
+
+// const b = prompt();
+// console.log(b);
+
+// let c = a + b;
+// console.log(c);
+
+const nome = prompt("Inserisci il tuo nome");
+console.log(nome);
+
+const cognome = prompt("Inserisci il tuo cognome");
+console.log(cognome);
+
+const colore = prompt("Inserisci il tuo colore preferito");
+console.log(colore);
+
+console.log(
+    `
+    ${nome}${cognome}${colore}21
+
+    `
+);
+
+
